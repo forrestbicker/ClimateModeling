736bacb79ec0bc6bc108e08efaf9e971ca11d83a
Forrest Bicker <bickerforrest@gmail.com>
2020-02-15 22:24:30 -0500

    fixed input reading

--- AI/MASTER_toJSON.py
+++ AI/MASTER_toJSON.py
@@ -4,7 +4,7 @@ import numpy as np
 #---------------------#
 # Specify input paths #
 #--------------A-------#
-climateNOAA_path = "/Users/forrestbicker/Documents/Code/Python/WorkInProgress/MTF/data/AI/RAW/ClimateVariablesNOAA.json"
+ClimateNOAA_path = "/Users/forrestbicker/Documents/Code/Python/WorkInProgress/MTF/data/AI/RAW/ClimateVariablesNOAA.json"
 OverallUSDA_path = "/Users/forrestbicker/Documents/Code/Python/WorkInProgress/MTF/data/AI/RAW/OverallUSDA.json"
 IndemnifiedUSDA_path = "/Users/forrestbicker/Documents/Code/Python/WorkInProgress/MTF/data/AI/RAW/IndemnifiedUSDA.json"
 
@@ -16,9 +16,14 @@ output_path = "/Users/forrestbicker/Documents/Code/Python/WorkInProgress/MTF/dat
 #-------#
 # Setup #
 #-------#
-climateNOAA_dict = load(climateNOAA_path)
-IndemnifiedUSDA_dict = load(IndemnifiedUSDA_path)
-OverallUSDA_dict = load(OverallUSDA_path)
+with open(ClimateNOAA_path, "r") as ClimateNOAA_file:
+    ClimateNOAA_dict = load(ClimateNOAA_file)
+
+with open(IndemnifiedUSDA_path, "r") as IndemnifiedUSDA_file:
+    IndemnifiedUSDA_dict = load(IndemnifiedUSDA_file)
+
+with open(OverallUSDA_path, "r") as OverallUSDA_file:
+    OverallUSDA_dict = load(OverallUSDA_file)
 
 MASTER_dict = {}
 # final JSON to be in the following form
@@ -40,7 +45,7 @@ for countyID in OverallUSDA_dict:
         for month in OverallUSDA_dict[countyID][year]:
             datapointID = countyID + year + month
 
-            inputData = climateNOAA_dict[countyID][year][month]
+            inputData = ClimateNOAA_dict[countyID][year][month]
 
             indemnifiedAcres = IndemnifiedUSDA_dict[countyID][year][month][0]
             totalAcres = IndemnifiedUSDA_dict[countyID][year][month][1]
