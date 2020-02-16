4f791afc0421d063e805a45669724446b7ea2116
Forrest Bicker <bickerforrest@gmail.com>
2020-02-15 21:57:09 -0500

    loaded jsons to dicts

--- AI/MASTER_toJSON.py
+++ AI/MASTER_toJSON.py
@@ -1,4 +1,9 @@
+from json import load, dump
+
 climateNOAA_path = "/Users/forrestbicker/Documents/Code/Python/WorkInProgress/MTF/data/static/ClimateVariablesNOAA.json"
 OverallUSDA_path = "/Users/forrestbicker/Documents/Code/Python/WorkInProgress/MTF/data/static/OverallUSDA.json"
 IndemnifiedUSDA_path = "/Users/forrestbicker/Documents/Code/Python/WorkInProgress/MTF/data/static/IndemnifiedUSDA.json"
 
+climateNOAA_dict = load(climateNOAA_path)
+OverallUSDA_dict = load(OverallUSDA_path)
+IndemnifiedUSDA_dict = load(IndemnifiedUSDA_path)
