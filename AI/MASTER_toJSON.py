1afe8200386c2a0a30b3d44962d2dc64f8d413ce
Forrest Bicker <bickerforrest@gmail.com>
2020-02-15 21:56:12 -0500

    supplied paths to three json imputs

---
+++ AI/MASTER_toJSON.py
@@ -0,0 +1,4 @@
+climateNOAA_path = "/Users/forrestbicker/Documents/Code/Python/WorkInProgress/MTF/data/static/ClimateVariablesNOAA.json"
+OverallUSDA_path = "/Users/forrestbicker/Documents/Code/Python/WorkInProgress/MTF/data/static/OverallUSDA.json"
+IndemnifiedUSDA_path = "/Users/forrestbicker/Documents/Code/Python/WorkInProgress/MTF/data/static/IndemnifiedUSDA.json"
+
